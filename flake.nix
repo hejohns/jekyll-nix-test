3b2ff8219bcfe9ae20058232e7a5b3def20ada9e
hejohns <hejohns@umich.edu>
2022-07-23 15:40:36 -0400

    init

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
-  description = "A very basic flake";
-
+  description = "Trying out nix; Attempting to package a jekyll site";
+  inputs = {
+      nixpkgs.url = "nixpkgs/";
+  };
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
